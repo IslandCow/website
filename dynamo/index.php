22fa913bec366cc28ef4249cad090a5688463a61
Michael Sweet <michael.r.sweet@gmail.com>
2016-07-15 14:50:57 -0400

    Initial changes for the new "one page" web site design.

--- dynamo/index.php
+++ dynamo/index.php
@@ -96,6 +96,7 @@ print("    <ul class=\"nav nav-pills\"><li role=\"presentation\"><a href=\"${htm
 -->
   </div>
 </div>
+<!--
 <div class="row pwg-panel pwg-alt-1">
   <div class="col-md-12 col-sm-12">
     <h1>Active Workgroups</h1>
@@ -118,6 +119,44 @@ print("    <ul class=\"nav nav-pills\"><li role=\"presentation\"><a href=\"${htm
     </ul>
   </div>
 </div>
+-->
+<div class="row pwg-panel pwg-alt-0">
+  <div class="col-md-12 col-sm-12">
+    <h1><a name="ABOUT">About the Printer Working Group</a></h1>
+    <p>The Printer Working Group (PWG) is a Program of the <a href="http://www.ieee-isto.org/">IEEE Industry Standard and Technology Organization (ISTO)</a> with members including printer and multi-function device manufacturers, print server developers, operating system providers, print management application developers, and industry experts. Originally founded in 1991 as the Network Printing Alliance, the PWG is chartered to make printers, multi-function devices, and the applications and operating systems supporting them work together better.</p>
+    <p>The PWG enjoys an open standards development process. Everyone is welcome to contribute to the development of our documents and standards, serve as editors, and participate in interoperability tests. Members may additionally serve as officers in the various working groups. Voting Members approve the documents and standards for publication and may serve as officers of the PWG.</p>
+    <p>If you have questions about the PWG, participation in PWG activities, or membership in the PWG, please contact the <a href="mailto:chair@pwg.org">PWG Chair</a> or any other officer.</p>
+    <ul class="nav nav-pills">
+      <li role="presentation"><a href="<?print($html_path);?>chair/index.html">Officers</a></li>
+      <li role="presentation"><a href="<?print($html_path);?>pwg-logos/members.html">Members</a></li>
+      <li role="presentation"><a href="#PARTICIPATING">Participating</a></li>
+      <li role="presentation"><a href="#JOINING">Joining the PWG</a></li>
+    </ul>
+  </div>
+</div>
+<div class="row pwg-panel pwg-alt-1">
+  <div class="col-md-12 col-sm-12">
+    <h1>Active Workgroups</h1>
+    <p>The PWG currently has two active workgroups:</p>
+
+    <h2>Internet Printing Protocol (IPP)</h2>
+    <p>The Internet Printing Protocol workgroup is developing standards for IPP-based multi-function services such as scanning and facsimile as well as 3D printing solutions using IPP.</p>
+    <ul class="nav nav-pills">
+      <li role="presentation"><a href="<?print($html_path);?>ipp/index.html">IPP Workgroup Page</a></li>
+      <li role="presentation"><a href="<?print($html_path);?>ipp/everywhere.html">IPP Everywhere<sup>&reg;</sup></a></li>
+      <li role="presentation"><a href="<?print($html_path);?>3d/index.html">3D Printing</a></li>
+    </ul>
+
+    <h2>Semantic Model (SM)</h2>
+    <p>The Semantic Model workgroup is responsible for the modeling of the services - Print, Copy, Scan, FaxIn/Out, Resource, System Control, and Transform hosted on Multifunction Devices and, more generally, Imaging Systems. Standardization supports interoperability of systems, devices, and services in local, enterprise, and cloud deployments enabling improved job submission, job management, remote administration, and support. The goal of the workgroup is to define a unified Semantic Model and set of abstract operations for the most common and essential system, service, and device features.</p>
+    <p>The Semantic Model workgroup is also engaged in several mapping activities to support AFP and JDF workflows and remote monitoring and management through DTMF CIM.</p>
+    <ul class="nav nav-pills">
+      <li role="presentation"><a href="<?print($html_path);?>sm/index.html">Semantic Model Workgroup Page</a></li>
+      <li role="presentation"><a href="<?print($html_path);?>sm/model.html">Semantic Model Overview</a></li>
+    </ul>
+  </div>
+</div>
+>>>>>>> Initial changes for the new "one page" web site design.
 <div class="row pwg-panel">
   <div class="col-md-12 col-sm-12">
     <h1><a name="PARTICIPATING">Participating</a></h1>
